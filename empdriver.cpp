3e9de27323b39eb91758da08379ea01de995e53c
David Klett <david.ger.klett@gmail.com>
2019-03-17 20:19:12 -0400

    added functions for empmaps.cpp

--- empdriver.cpp
+++ empdriver.cpp
@@ -12,6 +12,16 @@ int main()
 {
     vector<Employee> x;
     x = employees();
-    cout << x[7].id(); //works!
+    //cout << x[7].id(); //works!
+    map <int, vector<Employee> > suh;
+
+    suh = mapSalRange(x); //works!!!
+    int n = suh[50000].size();
+    for(int i = 0; i < n; ++i)
+    {
+        //cout << suh[50000][i].name();
+        //cout << endl;
+    }
+    printSalRange(suh); //works!
     return 0;
 }
--- empmaps.cpp
+++ empmaps.cpp
@@ -21,26 +21,20 @@ using namespace std;
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-
+#include <map>
 //function to split string:
-std::vector<std::string> split(std::string strToSplit, char delimeter)
-{
-    std::stringstream ss(strToSplit);
-    std::string item;
-    std::vector<std::string> splittedStrings;
-    while (std::getline(ss, item, delimeter))
-    {
-       splittedStrings.push_back(item);
-    }
-    return splittedStrings;
-}
 
+/*
+This function builds and returns a vector of employees using the Employee class file. You can prompt
+the user for the input file in this function. Give the user a nice error message if the input file does not
+exist and exit.
+*/
 vector<Employee> employees(){
     std::string x;
     std::cout << "Enter input file name:";
     std::cin >> x;
     //if input fails, have error code
-    std::ifstream in("testRecords.dat"); //use x variable instead of hardcoded string
+    std::ifstream in("records.dat"); //use x variable instead of hardcoded string
     std::vector <Employee> employees;
     std::string line;
     //Read the next line from the file until it reaches the end
@@ -61,4 +55,79 @@ vector<Employee> employees(){
     return employees;
 }
 
+/*
+This function builds and returns an ordered map. The key for this map is the employee’s department
+id. The department id is the first four most significant digits of the employee identifier. For example,
+a record in the records.dat file contains “882966 Etzel 120580” with 882966 as the
+employee id. The department this employee belongs to is 8829.
 
+*/
+std::map<int,vector<Employee>> mapEmpDept(vector<Employee> & emp)
+{
+    int employeeSize = emp.size();
+    std::map<int, vector<Employee>> myMap;
+
+    for(int i = 0; i < employeeSize; ++i)
+    {
+        stringstream ss;
+        int number = emp[i].id();
+        ss << number;
+        int firstFourNumbers = atoi(ss.str().substr(0, 4).c_str());
+        myMap[firstFourNumbers].push_back(emp[i]);
+    }
+
+    return myMap;
+}
+
+/*
+This function builds and returns an ordered map. The key for this map is related to the employee’s
+salary based on 10,000 range increments. If the employee’s salary is in the range of 40,000 to
+49,999, the key is 40000. For example, a record in the records.dat file contains
+“882966 Etzel 120580” with 120580 as the employee’s salary. The salary range for this
+employee is 120000. Thus, the range is in 10,000 increments. Range 0 takes values [0,10000) and
+the key for this is 0, range 10,000 takes values from [10000, 20000) and the key for this is 10000, …
+*/
+map<int,vector<Employee>> mapSalRange(vector<Employee> & emp)
+{
+    int employeeSize = emp.size();
+    std::map<int, vector<Employee>> myMap;
+
+    for(int i = 0; i < employeeSize; ++i)
+    {
+        stringstream ss;
+        int salary = emp[i].sal();
+        ss << salary;
+        int salRange = (salary/10000)*10000;
+        //cout << salRange << endl;
+        myMap[salRange].push_back(emp[i]);
+    }
+    return myMap;
+}
+
+/*
+This function prints the number of employees within each range using an ordered map. Afterwards,
+it print the salary range with the most number of employees and prints the number of employees in
+this range. Example output produced by this function is:
+ORDERED Map Salary Range with most employees: 130000 containing 6400
+employees.
+*/
+void printSalRange(map<int,vector<Employee>> & salRange)
+{
+    int mostEmp = 0; //initialize to zero
+    int mostEmpNumber = 0;
+    for(auto const& pair: salRange)
+    {
+        cout << "Salary Range: " << pair.first;
+        cout << " # employees is " << pair.second.size();
+        cout << endl;
+        if(pair.second.size() > mostEmpNumber)
+        {
+             mostEmp = pair.first;
+             mostEmpNumber = pair.second.size();
+        }
+
+    }
+    cout << endl;
+    cout << "The sal range with most employees is " << mostEmp;
+    cout << endl << "with " << mostEmpNumber << " employees.";
+}
